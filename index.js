5ea232a4cd74ebe6734e9096de6c6631b4c708d4
Vishal Junghare <91119787+Vishalvj13@users.noreply.github.com>
2025-07-16 19:25:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ exports.handler = async (event) => {
   console.log("Hello from CI/CD Lambda!");
   return {
     statusCode: 200,
-    body: "Deployed via CodePipeline!"
+    body: "Deployed via CodePipeline2!"
   };
 };
